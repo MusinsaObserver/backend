ab81ca32239d33ca2a92a6f944d252607847f459
jiwonkim <andrewkimswe@gmail.com>
2024-11-04 19:22:31 +0900

    fix:Update ProductResponseDto to include PriceHistoryDto list

--- src/main/java/observer/backend/dto/PriceHistoryDto.java
+++ src/main/java/observer/backend/dto/PriceHistoryDto.java
@@ -1,15 +1,18 @@
 package observer.backend.dto;
 
 import lombok.Getter;
-import observer.backend.entity.PriceHistory;
 import java.time.LocalDate;
+import observer.backend.entity.PriceHistory;
 
 @Getter
 public class PriceHistoryDto {
+	private Long id;
 	private LocalDate date;
 	private Integer price;
 
+	// Constructor to create a DTO from an entity
 	public PriceHistoryDto(PriceHistory priceHistory) {
+		this.id = priceHistory.getId();
 		this.date = priceHistory.getDate();
 		this.price = priceHistory.getPrice();
 	}
