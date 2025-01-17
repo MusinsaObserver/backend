210aa5da5733f7dbcd7cdc7aec00ad38ac93227d
김지원 <110761377+andrewkimswe@users.noreply.github.com>
2025-01-17 22:40:21 +0900

    release

--- src/main/java/observer/backend/service/CrawlerService.java
+++ src/main/java/observer/backend/service/CrawlerService.java
@@ -145,7 +145,7 @@ public class CrawlerService {
     return allResults;
 }
 
-    @Scheduled(cron = "0 56 19 * * ?")
+    @Scheduled(cron = "0 0 4 * * ?")
     public void scheduleCrawling() {
         log.info("Scheduled crawling started...");
         try {
